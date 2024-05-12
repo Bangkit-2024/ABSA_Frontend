58635db65f632f88d0b6358c728901d93ae81717
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2024-05-12 19:59:56 +0800

    feat(structure): Fix menu to match requirements

--- src/Routes/allRoutes.ts
+++ src/Routes/allRoutes.ts
@@ -28,6 +28,7 @@ import ComingSoon from "pages/AuthenticationInner/ComingSoon";
 import Maintenance from "pages/AuthenticationInner/Maintenance";
 import Offline from "pages/AuthenticationInner/Offline";
 import Pages404 from "pages/AuthenticationInner/Pages404";
+import Login from "pages/Authentication/Login";
 
 interface RouteObject {
   path: string;
@@ -70,7 +71,7 @@ const publicRoutes = [
 
 
   // authentication
-  { path: "/login", component: LoginModern },
+  { path: "/login", component: Login },
   { path: "/logout", component: Logout },
   { path: "/register", component: Register },
 
