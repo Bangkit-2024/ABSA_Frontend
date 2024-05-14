d0e77626e54e1dd7a94a62904585aa2ebde6eeab
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2024-05-14 22:30:59 +0800

    fix: :fire: Remove Unused files
    flag file etc

--- src/pages/Dashboards/Ecommerce/index.tsx
+++ src/pages/Dashboards/Ecommerce/index.tsx
@@ -1,15 +1,4 @@
 import React, { useEffect } from 'react';
-import BreadCrumb from 'Common/BreadCrumb';
-import WelcomeWidget from './WelcomeWidget';
-import OrderStatistics from './OrderStatistics';
-import Widgets from './Widgets';
-import SalesRevenue from './SalesRevenue';
-import TrafficResources from './TrafficResources';
-import ProductsOrders from './ProductsOrders';
-import CustomerService from './CustomerService';
-import SalesMonth from './SalesMonth';
-import TopSellingProducts from './TopSellingProducts';
-import Audience from './Audience';
 import { useNavigate } from 'react-router-dom';
 
 const Ecommerce = () => {
@@ -19,7 +8,6 @@ const Ecommerce = () => {
 
     return (
         <React.Fragment>
-            <BreadCrumb title='Dashboard' pageTitle='Dashboards' />
             <div className="grid grid-cols-12 gap-x-5">
             </div>
         </React.Fragment>
