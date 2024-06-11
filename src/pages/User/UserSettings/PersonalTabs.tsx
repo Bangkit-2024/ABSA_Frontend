6cd740608250f664fa9ebc2a62b30753a3c16953
Fajri <fajripra23@gmail.com>
2024-06-11 21:19:22 +0700

    refactor: :lipstick: Add UI Notification for personal tab
    Add notifications when there is a change to one or more of the data

--- src/pages/User/UserSettings/PersonalTabs.tsx
+++ src/pages/User/UserSettings/PersonalTabs.tsx
@@ -1,9 +1,50 @@
-import React from "react";
-import { ToastContainer, toast } from "react-toastify";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 const PersonalTabs = () => {
+    const [formValues, setFormValues] = useState({
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+        emailAddress: "",
+        birthDate: "",
+        joiningDate: "",
+        description: ""
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setFormValues({ ...formValues, [id]: value });
+    };
+    
+    const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { id, value } = e.target;
+        setFormValues({ ...formValues, [id]: value });
+    };
+    
+
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setFormValues({ ...formValues, [id]: value });
+    };
+
     const handleUpdateClick = () => {
+        const { firstName, lastName, phoneNumber, emailAddress, birthDate, joiningDate, description } = formValues;
+    
+        // Memeriksa apakah setidaknya satu input telah diubah dari nilai awalnya
+        if (!firstName && !lastName && !phoneNumber && !emailAddress && !description && !birthDate && !joiningDate) {
+            // Tampilkan notifikasi kesalahan
+            toast.error("Please update at least one field!", {
+                position: "top-right",
+                autoClose: 5000,
+                theme: "colored",
+                icon: false,
+                closeButton: false
+            });
+            return;
+        }
+    
         toast.success("Personal information updated successfully!", {
             position: "top-right",
             autoClose: 5000,
@@ -15,7 +56,6 @@ const PersonalTabs = () => {
 
     return (
         <React.Fragment>
-            <ToastContainer />
             <div className="card">
                 <div className="card-body">
                     <h6 className="mb-1 text-15">Personal Information</h6>
@@ -24,39 +64,82 @@ const PersonalTabs = () => {
                         <div className="grid grid-cols-1 gap-5 xl:grid-cols-12">
                             <div className="xl:col-span-6">
                                 <label htmlFor="inputValueFirst" className="inline-block mb-2 text-base font-medium">First Name</label>
-                                <input type="text" id="inputValueFirst" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter your value" defaultValue="Paula" />
+                                <input 
+                                    type="text" 
+                                    id="firstName" 
+                                    value={formValues.firstName}
+                                    onChange={handleInputChange}
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" 
+                                    placeholder="Enter your first name" 
+                                />
                             </div>
                             <div className="xl:col-span-6">
                                 <label htmlFor="inputValueLast" className="inline-block mb-2 text-base font-medium">Last Name</label>
-                                <input type="text" id="inputValueLast" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter your value" defaultValue="Keenan" />
+                                <input 
+                                    type="text" 
+                                    id="lastName" 
+                                    value={formValues.lastName}
+                                    onChange={handleInputChange}
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" 
+                                    placeholder="Enter your last name" 
+                                />
                             </div>
                             <div className="xl:col-span-6">
                                 <label htmlFor="inputValuePhone" className="inline-block mb-2 text-base font-medium">Phone Number</label>
-                                <input type="text" id="inputValuePhone" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="+214 8456 8459 23" defaultValue="+214 8456 8459 23" />
+                                <input 
+                                    type="number" 
+                                    id="phoneNumber" 
+                                    value={formValues.phoneNumber} 
+                                    onChange={handleInputChange}
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" 
+                                    placeholder="Enter your phone number" 
+                                />
                             </div>
                             <div className="xl:col-span-6">
                                 <label htmlFor="inputValueEmail" className="inline-block mb-2 text-base font-medium">Email Address</label>
-                                <input type="email" id="inputValueEmail" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter your email address" defaultValue="richardmarshall@tailwick.com" />
+                                <input 
+                                    type="email" 
+                                    id="emailAddress" 
+                                    value={formValues.emailAddress} 
+                                    onChange={handleInputChange}
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" 
+                                    placeholder="Enter your email address" 
+                                />
                             </div>
                             <div className="xl:col-span-6">
-                                <label htmlFor="joiningDateInput" className="inline-block mb-2 text-base font-medium">Birth of Date</label>
-                                <input type="date" name="" id="" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"/>
+                            <label htmlFor="birthDateInput" className="inline-block mb-2 text-base font-medium">Birth of Date</label>
+                            <input 
+                                type="date" 
+                                id="birthDate" 
+                                onChange={handleDateChange} 
+                                className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                                required  // Menjadikan input ini wajib diisi sebelum formulir dapat dikirim
+                            />
                             </div>
                             <div className="xl:col-span-6">
                                 <label htmlFor="joiningDateInput" className="inline-block mb-2 text-base font-medium">Joining Date</label>
-                                <input type="date" name="" id="" className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"/>
+                                <input 
+                                    type="date" 
+                                    id="joiningDate" 
+                                    onChange={handleDateChange} 
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                                    required  // Menjadikan input ini wajib diisi sebelum formulir dapat dikirim
+                                />
                             </div>
                             <div className="xl:col-span-12">
                                 <label htmlFor="exampleFormControlTextarea" className="block mb-2 text-base font-medium">Description</label>
-                                <textarea className="w-full form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" id="exampleFormControlTextarea" placeholder="Enter your description" rows={5}
-                                    defaultValue="
-                                A Web Developer creates and designs different websites for clients. They are responsible for their aesthetic as well as their function. Professionals in this field may also need to be able to ensure sites are compatible with multiple types of media. Web Developers need to have a firm understanding of programming and graphical design. Having a strong resume that emphasizes these attributes makes it significantly easier to get hired as a Web Developer. As a web designer, my objective is to make a positive impact on clients, co-workers, and the Internet using my skills and experience to design compelling and attractive websites. Solving code problems. Editing &amp; Design with designing team in the company to build perfect web designs.">
-                                </textarea>
+                                <textarea 
+                                    className="w-full form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" 
+                                    id="description" 
+                                    // value={formValues.description} 
+                                    onChange={handleTextareaChange} 
+                                    placeholder="Enter your description here..." 
+                                    rows={5}
+                                    />
                             </div>
                         </div>
                         <div className="flex justify-end mt-6 gap-x-4">
-                            <button type="button" className="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20" onClick={handleUpdateClick}>Updates</button>
-                            <button type="button" className="text-red-500 bg-red-100 btn hover:text-white hover:bg-red-600 focus:text-white focus:bg-red-600 focus:ring focus:ring-red-100 active:text-white active:bg-red-600 active:ring active:ring-red-100 dark:bg-red-500/20 dark:text-red-500 dark:hover:bg-red-500 dark:hover:text-white dark:focus:bg-red-500 dark:focus:text-white dark:active:bg-red-500 dark:active:text-white dark:ring-red-400/20">Cancel</button>
+                            <button type="button" className="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20" onClick={handleUpdateClick}>Update</button>
                         </div>
                     </form>
                 </div>
